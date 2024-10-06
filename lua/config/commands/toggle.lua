3a1a375980831b829ecbeeefc39217fe5a197e00
p1an1st <alex.hanimar@gmail.com>
2024-10-06 16:56:11 +0300

    Update toggle menu

--- lua/config/commands/toggle.lua
+++ lua/config/commands/toggle.lua
@@ -1,10 +1,30 @@
 local M = {}
 
+local ch_tabwidth = function ()
+	vim.ui.select(
+		{"1", "2", "3", "4"},
+		{
+			prompt = "select tab width",
+			format_item = function (val)
+				return ""..val
+			end
+		},
+		function (choice)
+			if choice ~= nil then
+				vim.cmd("set shiftwidth="..choice)
+				vim.cmd("set tabstop="..choice)
+				vim.notify("set tab width to "..choice)
+			end
+		end
+	)
+end
+
 M.toggle_menu = function ()
 	vim.ui.select(
 		{
 			"wrap",
 			"expandtab",
+			"tab width",
 		},
 		{
 			prompt = "Change option:",
@@ -12,9 +32,13 @@ M.toggle_menu = function ()
 		},
 		function (choice)
 			if choice == "wrap" then
-				vim.o.wrap = not vim.o.wrap
+				vim.cmd("set wrap!")
+				vim.notify("set wrap to "..tostring(vim.o.wrap))
 			elseif choice == "expandtab" then
-				vim.o.expandtab = not vim.o.expandtab
+				vim.cmd("set expandtab!")
+				vim.notify("set expandtab to "..tostring(vim.o.expandtab))
+			elseif choice == "tab width" then
+				ch_tabwidth()
 			end
 		end
 	)
